a7e5d64a4fc12249efc7171225e00e104aed349a
prabhakaran <prabhakaranplkk@gmail.com>
2023-08-24 22:08:23 +0530

    leap year

---
+++ css/main.js
@@ -0,0 +1,10 @@
+var n=parseInt(prompt("Enter the year"));
+if(n%4===0){
+    console.log(n+" "+"is a leap year");
+}
+else if(n%4!==0){
+    console.log(n+" "+"not leap year");
+}
+else{
+    console.log("Enter the number");
+}
